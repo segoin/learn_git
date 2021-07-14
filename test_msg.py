55ab084ee1497e605985bd01e53ef9a060c9f95c
segoin <80241950+segoin@users.noreply.github.com>
2021-07-14 21:30:07 +0900

    Update test_msg.py

--- test_msg.py
+++ test_msg.py
@@ -1,7 +1,5 @@
-import requests 
-def send_message_to_slack(text): 
-    url = "https://hooks.slack.com/services/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" 
-    payload = { "text" : text } 
-    requests.post(url, json=payload)
+def incomingWebhookMessage(message):
+    slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+    slack.notify(text=message, channel="#auto_trade",username="bot",mrkdwn=True)
 
-send_message_to_slack("AWS Send Message Using Python")
+incomingWebhookMessage("aws testss_please")
