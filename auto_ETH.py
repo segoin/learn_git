9e72ab570a88772d762aa391d6cecaef02b0ef9d
segoin <80241950+segoin@users.noreply.github.com>
2021-07-14 14:18:41 +0900

    Update auto_ETH.py

--- auto_ETH.py
+++ auto_ETH.py
@@ -13,18 +13,5 @@ access = "ccyUip4ujykayb5qFP9xkncrbbt9HPM98vtVQeqV"
 secret = "x3fUq5n4MMHDyKDKeNZdVOcoMjiWQqB0pBBErDjn"
 myToken = "xoxb-1702501177444-1689568357894-eTntzP8uGJlB0sDjbpfDP5mK"
 
-def MinMaxScaler(data):
-    nu  =   data - np.min(data,0)
-    de  =   np.max(data,0) - np.min(data,0)
-    return nu / (de+1e-7)
-
-def Light_GBM(ticker):
-    # OHLCV(open, high, low, close, volume)로 당일 시가,고가,저가,종가,거래량에 대한 데이터
-    df = pyupbit.get_ohlcv(ticker, interval="minutes30", count=10000)
-
-    # 익일종가
-    df['close_nextday'] = df['close'].shift(-1)
-    df['volume_mean_3day'] = df['volume'].rolling(window=3).mean()
-    df['vloume_inc_yn_vs_last_1day'] = np.where(df['volume'] > df['volume_last_1day'],1,0)
-    df['bol_upper'] = df['MA20'] + ( 2 * df['close'].rolling(window=20).std() )
+post_message(myToken,"#stock", "autotrade start")
 
